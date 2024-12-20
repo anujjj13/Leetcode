55731f1878e174a9ae710b23f6ab0cfff005c6e0
anujjj13 <anujsharma1913@gmail.com>
2024-12-20 19:57:35 +0530

    Time: 0 ms (100%), Space: 42.9 MB (81.65%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int pivot = -1;
+        int n = nums.length - 1;
+
+        // Step 1: Find the first decreasing element from the end
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // If no such element is found, that means the array is sorted in descending order
+        if (pivot == -1) {
+            Arrays.sort(nums);
+            return;
+        }
+
+        // Step 2: Find the element just larger than nums[pivot] from the right
+        for (int i = n; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                // Swap the elements
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+
+        // Step 3: Reverse the portion of the array from pivot+1 to end
+        int i = pivot + 1, j = n;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
