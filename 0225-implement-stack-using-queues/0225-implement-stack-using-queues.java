fc9f3c6ee060f1f9f03519d88b116ea5ba37e297
anujjj13 <anujsharma1913@gmail.com>
2025-03-21 21:50:07 +0530

    Time: 0 ms (100%), Space: 41.4 MB (64.32%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,42 +1,45 @@
 class MyStack {
     private Queue<Integer> q1;
     private Queue<Integer> q2;
-
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q1=new LinkedList<>();
+        q2=new LinkedList<>();
     }
-
+    
     public void push(int x) {
-        // Push to q2
         q2.add(x);
-        
-        // Move all elements from q1 to q2
-        while (!q1.isEmpty()) {
+        while(!q1.isEmpty()){
             q2.add(q1.poll());
         }
-        
-        // Swap q1 and q2
         Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
+        q1=q2;
+        q2=temp;
     }
-
+    
     public int pop() {
-        if (q1.isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+        if(q1.isEmpty()){
+            throw new IllegalStateException("empty");
         }
         return q1.poll();
     }
-
+    
     public int top() {
-        if (q1.isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+        if(q1.isEmpty()){
+            throw new IllegalStateException("empty");
         }
         return q1.peek();
     }
-
+    
     public boolean empty() {
         return q1.isEmpty();
     }
 }
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
